d7e9cc5031d93f64677de874900a4ed12878aec4
Magne Alvheim <magne@alvheim.com>
2022-02-02 14:23:44 +0100

    react-faceapi-fasit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import * as faceapi from 'face-api.js'
+import { FaceMatcherComponent } from "./FaceMatcherComponent"
+
 
 function App() {
+  
+  const KNOWN_FACES = {
+    names: ['Magne'],
+    folder: 'training',
+    ext: 'png', 
+  }
+
+  const [faceMatcher, setFaceMatcher] = useState()
+  const [isReady, setIsReady] = useState(false)
+
+  useEffect(() => {
+
+    async function initialiseFaceApi() {    
+  
+      await Promise.all([
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+        faceapi.nets.ageGenderNet.loadFromUri('/models')
+      ]).then(console.log('FaceApi is loaded..'))
+
+      console.log('Start training model with known faces')      
+      const faceDescriptors = await trainModelWithKnownFaces(KNOWN_FACES)
+      const facMat = new faceapi.FaceMatcher(faceDescriptors, 0.6)
+      setFaceMatcher(facMat)
+      console.log('Done training model with known faces')
+
+      setIsReady(true)
+                
+    };
+    initialiseFaceApi()
+  
+  }, [])
+
+  function trainModelWithKnownFaces(personList) {
+
+    return Promise.all(
+      personList.names.map(async (name) => {
+        
+        const faceDescriptors = []        
+        let personProcessed = false
+        let i = 0
+        
+        while (!personProcessed) {
+          
+          i++
+          
+          console.log(`Learning the face of ${name} version ${i}.`)          
+          
+          try {
+
+            const image = await faceapi
+              .fetchImage(`${personList.folder}/${name}/${i}.${personList.ext}`)
+
+            const detections = await faceapi
+              .detectSingleFace(image)
+              .withFaceLandmarks()
+              .withFaceDescriptor()
+            
+            if (detections) {
+
+              console.log(detections)              
+              faceDescriptors.push(detections.descriptor)              
+              console.log(`Loaded image: ${name}/${i}.jpg`)  
+            }
+            
+
+          } catch (error) {
+            personProcessed = true
+          }               
+        }
+  
+        // Returnere fra promise med array over detektert ansikt fra alle bildene for en person
+        return new faceapi.LabeledFaceDescriptors(name, faceDescriptors)
+      })
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!isReady && <p>Please Wait. Initializing the face api.</p>}      
+      {isReady && <FaceMatcherComponent faceMatcher={faceMatcher} />}
     </div>
   );
 }
---
+++ src/FaceMatcherComponent.js
@@ -0,0 +1,63 @@
+import React, { useRef } from "react";
+import * as faceapi from 'face-api.js';
+
+export const FaceMatcherComponent = (props) => {
+
+    console.log("facematchercomp")
+
+    const imageRef = useRef();
+    const canvasRef = useRef(null);
+  
+    const faceMatcher = props.faceMatcher
+  
+    const imageOnChange = async (event) => {
+  
+     
+        let image = await faceapi.bufferToImage(event.target.files[0])
+        imageRef.current.src = image.src
+
+        let canvas = canvasRef.current
+        canvasRef.current = faceapi.createCanvasFromMedia(image)
+  
+        const displaySize = { width: image.width, height: image.height }
+        faceapi.matchDimensions(canvas, displaySize) 
+        canvasRef.current = canvas
+  
+        const detections = await faceapi
+            .detectAllFaces(image)
+            .withFaceLandmarks()
+            .withAgeAndGender()
+            .withFaceDescriptors()
+        
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        const results = resizedDetections
+            .map(d => faceMatcher.findBestMatch(d.descriptor))
+      
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi
+                .draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+        })        
+    }
+
+    return (
+        <>
+            <div>
+                <input type="file" onChange={(event) => imageOnChange(event)} />
+            </div>
+            <div>
+                <img
+                    id="imageUpload"                     
+                    ref={imageRef}
+                    style={{ position: "absolute", top: 50, left: 0 }}
+                />
+                <canvas
+                    ref={canvasRef}
+                    style={{ position: "absolute", top: 50, left: 0 }}     
+                />
+      
+            </div>
+        </>
+    )
+}
